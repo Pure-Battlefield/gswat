709da693811a5163da3bf660d73d3dc0607b2e8f
Conner Davis <connergdavis@gmail.com>
2012-12-21 15:06:36 -0800

    Added chat auto-refresh as well as force-refresh button

--- WebFrontend/Scripts/ajaxUpdateScript.js
+++ WebFrontend/Scripts/ajaxUpdateScript.js
@@ -1,21 +1,32 @@
 ﻿function loadMessageQueue()
 {
+    xmlHttpRequestMessageQueue();
+
+    window.setInterval(function () {
+        xmlHttpRequestMessageQueue();
+    }, 10000 /* A.K.A. 10 seconds */);
+}
+
+
+// Avoid having to write the same code twice
+// TODO Change the name of this function to something else that's not as long
+function xmlHttpRequestMessageQueue() {
+    // Testing purposes
+    console.log('Refreshing..');
+
     var xmlhttp;
-    if (window.XMLHttpRequest)
-    {// code for IE7+, Firefox, Chrome, Opera, Safari
-        xmlhttp=new XMLHttpRequest();
+    if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
     }
-    else
-    {// code for IE6, IE5
-        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    else {// code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
-    xmlhttp.onreadystatechange=function()
-    {
-        if (xmlhttp.readyState==4 && xmlhttp.status==200)
-        {
-            document.getElementById("chatContents").innerHTML=xmlhttp.responseText;
+
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            document.getElementById("chatContents").innerHTML = xmlhttp.responseText;
         }
     }
-    xmlhttp.open("GET","/api/values",true);
+    xmlhttp.open("GET", "/api/values", true);
     xmlhttp.send();
 }
