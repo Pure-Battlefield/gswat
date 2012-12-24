2e6b6e30e9a6cb07af740d442962895bcc558ed0
Conner Davis <connergdavis@gmail.com>
2012-12-23 23:31:08 -0800

    Huge amount of code-cleanup/renaming to frontend

--- WebFrontend/Scripts/ajaxUpdateScript.js
+++ WebFrontend/Scripts/ajaxUpdateScript.js
@@ -1,11 +1,21 @@
-﻿var parser;
+﻿/*
+    The last known JSON parser, which is what sets the chat refresh interval.
+    We need this to be traceable because when the refresh interval is changed,
+    the previous interval must be killed.
+ */
+var parser;
 
+// Whether or not to continuously load chat messages.  Disabled for historical chat message loading.
 var loop;
 
+/**
+ * Load the queue of chat messages.
+ * 
+ * @param interval      How often to refresh the queue.
+ */
 function loadMessageQueue(interval)
 {
-    // Default is 1 second
-    if (interval === undefined) {
+    if (interval === undefined) { // Set the interval to the default if it's not defined explicitly.
         interval = 1;
     }
 
@@ -13,38 +23,60 @@ function loadMessageQueue(interval)
         window.clearInterval(parser);
     }
 
+    // Since we're loading at a periodic interval, definitely need to loop loading.
     loop = true;
-    parseJSON();
-    parser = window.setInterval(parseJSON, interval * 1000);
-    console.log('Chat refresh rate set to ' + interval + ' seconds');
+
+    getCurrentChat();
+
+    // Set the parser to load at the interval specified.
+    parser = window.setInterval(getCurrentChat, interval * 1000);
 }
 
 
-// Avoid having to write the same code twice
-function parseJSON() {
-    if (loop == true) {
-        $.get('/api/values/getallmessages', function(data) {
-            data = JSON.parse(data);
+// Get all current chat messages.
+function getCurrentChat()
+{
+    $.get('/api/values/getallmessages', function (data) {
+        parseJSON(data);
+    });
+}
 
-            var content = "";
-            for (var message in data) {
-                var timestamp = moment(new Date(parseInt(data[message].MessageTimeStamp.replace('/Date(', ''))));
-                timestamp = timestamp.format("MM/DD/YYYY HH:mm:ss");
-                var channel = data[message].MessageType.toUpperCase();
-                var speaker = data[message].Speaker;
-                var text = data[message].Text;
 
-                var stmt = "<p>[%ts] [%c] <strong>%s</strong>: %t</p>";
-                //start conditionals for each message (all, squad team etc)
+/**
+ * Highly re-used:  parse the JSON data sent by the server which details chat messages.
+ *
+ * @param data      Raw data sent by the server to be decoded as JSON.
+ */
+function parseJSON(data) {
+    // Parse the data into JSON to be decoded.
+    data = JSON.parse(data);
 
-                stmt = stmt.replace('%ts', timestamp)
-                    .replace('%c', channel)
-                    .replace('%s', speaker)
-                    .replace('%t', text);
-                content += stmt;
-            }
-            $('#chatContents').empty().append(content);
-        });
-        console.log('Refreshing chat..');
+    var content = '';
+    for (var message in data) { // Loop through each chat message in the data sent
+        // The timestamp hooked to the current chat message.
+        var timestamp = moment(new Date(parseInt(data[message].MessageTimeStamp.replace('/Date(', ''))));
+        timestamp = timestamp.format('MM/DD/YYYY HH:mm:ss');
+
+        // The channel this user is in (ALL, SQUAD#, etc.)
+        var channel = data[message].MessageType.toUpperCase();
+
+        // The speaker, or user saying the message.
+        var speaker = data[message].Speaker;
+
+        // The message body.
+        var text = data[message].Text;
+
+        // Prepare the HTML statement to be written.
+        var stmt = '<p>[%ts] [%c] <strong>%s</strong>: %t</p>';
+        stmt = stmt.replace('%ts', timestamp)
+            .replace('%c', channel)
+            .replace('%s', speaker)
+            .replace('%t', text);
+
+        // Append the prepared statement to 'content', which is the chunk of HTML data of all the messages.
+        content += stmt;
     }
+    
+    // Clean out the chat div and add the new stuff.  This is significantly faster than it sounds it would be.
+    $('#chatContents').empty().append(content);
 }
