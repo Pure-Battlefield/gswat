92173924b033558e30e2069edb524db4c9b53027
William Barr <william.barr@gatech.edu>
2013-01-02 19:41:49 -0500

    Added Team/Squad replacement logic
    Now uses appropriate team/squad names in the downloadable log file.

--- WebFrontend/Controllers/ValuesController.cs
+++ WebFrontend/Controllers/ValuesController.cs
@@ -14,6 +14,46 @@ namespace WebFrontend.Controllers
 {
     public class ValuesController : ApiController
     {
+        private static readonly Dictionary<string, string> Squads;
+        private static readonly Dictionary<string, string> Teams; 
+
+        static ValuesController()
+        {
+            Squads = new Dictionary<string, string>();
+            Teams = new Dictionary<string, string>();
+
+            Teams["TEAM1"] = "US";
+            Teams["TEAM2"] = "RU";
+
+            Squads.Add("SQUAD0", " UNSQUADDED");
+            Squads.Add("SQUAD1", " ALPHA");
+            Squads.Add("SQUAD2", " BRAVO");
+            Squads.Add("SQUAD3", " CHARLIE");
+            Squads.Add("SQUAD4", " DELTA");
+            Squads.Add("SQUAD5", " ECHO");
+            Squads.Add("SQUAD6", " FOXTROT");
+            Squads.Add("SQUAD7", " GOLF");
+            Squads.Add("SQUAD8", " HOTEL");
+            Squads.Add("SQUAD9", " INDIA");
+            Squads.Add("SQUAD10", " JULIET");
+            Squads.Add("SQUAD11", " KILO");
+            Squads.Add("SQUAD12", " LIMA");
+            Squads.Add("SQUAD13", " MIKE");
+            Squads.Add("SQUAD14", " NOVEMBER");
+            Squads.Add("SQUAD15", " OSCAR");
+            Squads.Add("SQUAD16", " PAPA");
+            Squads.Add("SQUAD17", " QUEBEC");
+            Squads.Add("SQUAD18", " ROMEO");
+            Squads.Add("SQUAD19", " SIERRA");
+            Squads.Add("SQUAD20", " TANGO");
+            Squads.Add("SQUAD21", " UNIFORM");
+            Squads.Add("SQUAD22", " VICTOR");
+            Squads.Add("SQUAD23", " WHISKEY");
+            Squads.Add("SQUAD24", " XRAY");
+            Squads.Add("SQUAD25", " YANKEE");
+            Squads.Add("SQUAD26", " ZULU");
+        }
+
         [HttpGet]
         [ActionName("GetAllMessages")]
         public string GetAllMessages()
@@ -35,6 +75,16 @@ namespace WebFrontend.Controllers
 
             foreach (var message in q)
             {
+                message.MessageType = message.MessageType.ToUpperInvariant();
+                foreach (var teamPair in Teams)
+                {
+                    message.MessageType = message.MessageType.Replace(teamPair.Key, teamPair.Value);
+                }
+
+                foreach (var squadPair in Squads)
+                {
+                    message.MessageType = message.MessageType.Replace(squadPair.Key, squadPair.Value);
+                }
 
                 writer.Write(String.Format(messageFmt, message.MessageTimeStamp, message.MessageType, message.Speaker,
                                   message.Text) + "\n");
@@ -44,7 +94,11 @@ namespace WebFrontend.Controllers
 
             HttpResponseMessage result = new HttpResponseMessage(HttpStatusCode.OK);
             result.Content = new StreamContent(stream);
-            result.Content.Headers.ContentType = new MediaTypeHeaderValue("text/octet-stream");
+            result.Content.Headers.ContentType = new MediaTypeHeaderValue("text/plain");
+            var disposition = new ContentDispositionHeaderValue("attachment");
+            disposition.FileName = string.Format("{0}.txt", temp.ToString("yyyyMMdd"));
+
+            result.Content.Headers.ContentDisposition = disposition;
 
             return result;
         }
