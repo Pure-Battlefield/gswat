b3e0cea5045dde7195ceab03c261accf21e48ba2
Enzo Martin <enzo.r.martin@gmail.com>
2013-02-16 23:58:57 +0100

    Fixed incorrect destroy methods - Fixed the destroy methods for views/collections/models as they weren't resetting them correctly - Moved modal and alert into their own views - Added models for footer/header

--- WebFrontend/Scripts/libraries/lib.js
+++ WebFrontend/Scripts/libraries/lib.js
@@ -105,43 +105,13 @@ _.extend(window,{
         },
 
         alert: function (alert_data) {
-            var header = this.get({ view: { name: 'header' } });
-            var type;
-            var title;
-            var message;
-            var alert_type = (typeof alert_data === 'string') ? alert_data : alert_data.type;
-            switch (alert_type) {
-                case 'success':
-                    type = 'success';
-                    title = 'Success!';
-                    message = 'Action completed successfully!';
-                    break;
-                case 'warning':
-                    type = 'block';
-                    title = 'Warning!';
-                    message = 'Issue detected!';
-                    break;
-                case 'error':
-                    type = 'error';
-                    title = 'Error!';
-                    message = 'An error occurred!';
-                    break;
-                case 'info':
-                    type = 'info';
-                    title = 'Info:';
-                    message = 'This is an informational message';
-                    break;
-            }
-
-            alert_data.type = alert_data.type || type;
-            alert_data.title = alert_data.title || title;
-            alert_data.message = alert_data.message || message;
+            var header = this.get({view:{name:'header'}});
             header.trigger('alert', alert_data);
         },
 
         modal: function (data, callback) {
-            var modal = this.get({ view: { name: 'modal' }, model: { name: 'modal', data: data, options: { callback: callback } } });
-            modal.show_modal();
+            var footer = this.get({view:{name:'footer'}});
+            footer.trigger('modal',data,callback);
         }
     };
 }(window, document, jQuery, _, yepnope));
